80ed13e5aaee31026643ecfe2bf38d677668a6e0
Ashutosh <Ashutosh>
2024-06-09 15:00:34 +0530

    Added base project to build RESTful APIs

---
+++ REST Fundamentals/server/src/features/items/items.router.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { getItems } from "./items.service";
+
+export const itemsRouter = express.Router();
+
+itemsRouter.get('/', async (req, res) => {
+  const items = await getItems();
+  items.forEach(item => {
+    item.imageUrl = buildImageUrl(req, item.id);
+  });
+  res.json(items);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+function buildImageUrl(req: any, id: number): string {
+  return `${req.protocol}://${req.get("host")}/images/${id}.jpg`;
+}
