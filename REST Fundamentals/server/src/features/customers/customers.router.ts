9da26521c1db760187ccc671676f9e81384c9d0e
Ashutosh <Ashutosh>
2024-06-09 15:08:59 +0530

    Created get customer list end point

--- REST Fundamentals/server/src/features/customers/customers.router.ts
+++ REST Fundamentals/server/src/features/customers/customers.router.ts
@@ -1,3 +1,9 @@
 import express from "express";
+import { getCustomers } from "./customers.service";
 
 export const customersRouter = express.Router();
+
+customersRouter.get('/', async (req, res) => {
+    const customers = await getCustomers();
+    res.json(customers);
+});
