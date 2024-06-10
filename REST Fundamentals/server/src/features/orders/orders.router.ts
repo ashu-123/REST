e217a256f1fa9c9be1b849289c59c03321b39be8
Ashutosh <Ashutosh>
2024-06-10 12:53:05 +0530

    Added delete end points

--- REST Fundamentals/server/src/features/orders/orders.router.ts
+++ REST Fundamentals/server/src/features/orders/orders.router.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { getOrders, upsertOrder } from "./orders.service";
+import { addOrderItems, getOrders, upsertOrder, deleteOrderItem, deleteOrder } from "./orders.service";
 import { validate } from "../../middleware/validation.middleware";
-import { orderPOSTRequestSchema } from "../types";
+import { idItemIdUUIDRequestSchema, idUUIDRequestSchema, orderItemsDTORequestSchema, orderPOSTRequestSchema } from "../types";
+import { create } from "xmlbuilder2";
 
 export const ordersRouter = express.Router();
 
@@ -36,3 +37,47 @@ ordersRouter.get("/", async (req, res) => {
             res.status(500).json({message: 'Creation failed'});
         }
     });
+
+    ordersRouter.post("/:id/items", validate(orderItemsDTORequestSchema), async (req, res) => {
+        const data = orderItemsDTORequestSchema.parse(req);
+        const order = await addOrderItems(data.params.id, data.body);
+
+        if(order!=null) res.status(201).json(order);
+        else res.status(500).json({message: "Creation failed"});
+    });
+
+    ordersRouter.delete("/:id", validate(idUUIDRequestSchema), async (req, res) => {
+        const data = idUUIDRequestSchema.parse(req);
+        const order = await deleteOrder(data.params.id);
+        if (order != null) {
+          res.json(order);
+        } else {
+          res.status(404).json({ message: "Order Not Found" });
+        }
+      });
+
+    ordersRouter.delete(
+        "/:id/items/:itemId",
+        validate(idItemIdUUIDRequestSchema),
+        async (req, res) => {
+          const data = idItemIdUUIDRequestSchema.parse(req);
+          const order = await deleteOrderItem(data.params.id, data.params.itemId);
+          if (order != null) {
+            if (req.headers["accept"] == "application/xml") {
+              res.status(201).send(create().ele("order", order).end());
+            } else {
+              res.status(201).json(order);
+            }
+          } else {
+            if (req.headers["accept"] == "application/xml") {
+              res
+                .status(404)
+                .send(
+                  create().ele("error", { message: "Order or item not found" }).end()
+                );
+            } else {
+              res.status(404).json({ message: "Order or item not found" });
+            }
+          }
+        }
+      );
