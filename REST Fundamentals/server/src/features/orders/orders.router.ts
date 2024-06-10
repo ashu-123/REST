16d8a31dd1a6e68b2cb85110fb0f10f7a0c24d4e
Ashutosh <Ashutosh>
2024-06-10 12:09:11 +0530

    Added pagination to api

--- REST Fundamentals/server/src/features/orders/orders.router.ts
+++ REST Fundamentals/server/src/features/orders/orders.router.ts
@@ -1,3 +1,24 @@
 import express from "express";
+import { getOrders } from "./orders.service";
 
 export const ordersRouter = express.Router();
+
+ordersRouter.get("/", async (req, res) => {
+    const query = req.query;
+    const take = query.take;
+    const skip = query.skip;
+
+    if(take &&
+        typeof take === "string" &&
+        parseInt(take) > 0 &&
+        skip &&
+        typeof skip === "string" &&
+        parseInt(skip) > -1) { 
+            const orders = await getOrders(parseInt(skip), parseInt(take));
+                res.json(orders);
+            }
+            else {
+                res.status(404).json({message: 'Not happening!'})
+            }
+        }
+    );
